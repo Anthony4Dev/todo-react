e4993c32f170d2f0b756b1de098088937bd50fda
Anthony4dev <anthony.islan2005@gmail.com>
2024-04-09 15:28:21 -0300

    Ultimo commit antes do deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 import { useState, useEffect } from "react";
-import { BsTrash, Bsmark, BsBookCheckFill, BsBookmarkCheck, BsBookmarkCheckFill } from "react-icons/bs";
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from "react-icons/bs";
 
 const API = "http://localhost:5000";
 
